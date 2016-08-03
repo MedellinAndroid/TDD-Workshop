23dba59bcf74e7df6190f4aae8b492341e351a15
Cristian Gomez <cristianfgr@gmail.com>
2016-08-03 05:30:17 -0500

    Paso 2: Test  de verificación de datos

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ patrón de diseño MVP (Model View Presenter).
 * El test debera validar mediante verificaciones de mockito que el comportamiento es correcto
 * la primera verificación es la mas simple, que al dar click en el boton de login se muestre la 
 pantalla de carga
+
+### Paso 2
+* Escribir el test en el cual el presentador chequea la interaccion con el caso de uso y este falla
+* Se usan los ArgumentCaptors para simular los llamados de los colaboradores
--- app/src/test/java/co/mde/android/tddexample/LoginPresenterTest.java
+++ app/src/test/java/co/mde/android/tddexample/LoginPresenterTest.java
@@ -2,6 +2,8 @@ package co.mde.android.tddexample;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -11,6 +13,7 @@ public class LoginPresenterTest {
   //Collaborators
   @Mock LoginView view;
   @Mock LoginInteractor interactor;
+  @Captor ArgumentCaptor<LoginInteractor.Callback> captor;
 
   @Before public void setup() {
     MockitoAnnotations.initMocks(this);
@@ -25,5 +28,22 @@ public class LoginPresenterTest {
     presenter.login();
     //Then
     Mockito.verify(view).showLoading();
+  }
+
+  @Test public void showLoginScreenAndErrorOnInteractorFailure() {
+    //Given
+    LoginPresenter presenter = new LoginPresenter(view, interactor);
+    Mockito.when(view.username()).thenReturn("non-registered@example.com");
+    Mockito.when(view.password()).thenReturn("invalid");
+    //When
+    presenter.login();
+    Mockito.verify(interactor)
+        .auth(Mockito.eq("non-registered@example.com"), Mockito.eq("invalid"), captor.capture());
+    captor.getValue().onFailure();
+
+    //Then
+    Mockito.verify(view).showLogin();
+    Mockito.verify(view).hideLoading();
+    Mockito.verify(view).showLoginError();
   }
 }
