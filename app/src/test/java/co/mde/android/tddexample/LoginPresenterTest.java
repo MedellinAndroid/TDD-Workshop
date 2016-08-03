952c48dec9f6d2ac11accf23368cdc9169259b21
Cristian Gomez <cristianfgr@gmail.com>
2016-08-03 05:15:02 -0500

    Paso 1a: Primer test de verificación al dar click en login

--- app/src/test/java/co/mde/android/tddexample/LoginPresenterTest.java
+++ app/src/test/java/co/mde/android/tddexample/LoginPresenterTest.java
@@ -3,6 +3,7 @@ package co.mde.android.tddexample;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 public class LoginPresenterTest {
@@ -16,6 +17,13 @@ public class LoginPresenterTest {
   }
 
   @Test public void showLoadingOnClick() throws Exception {
+    //Given
     LoginPresenter presenter = new LoginPresenterTest(view, interactor);
+    Mockito.when(view.username()).thenReturn("registered@example.com");
+    Mockito.when(view.password()).thenReturn("valid");
+    //When
+    presenter.login();
+    //Then
+    Mockito.verify(view).showLoading();
   }
 }
