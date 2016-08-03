43aaab5a720d1411d2d3d71bb3d8a898853c331f
Cristian Gomez <cristianfgr@gmail.com>
2016-08-03 05:32:07 -0500

    Paso 2b: resolución de errores de callbacks

--- app/src/main/java/co/mde/android/tddexample/LoginInteractor.java
+++ app/src/main/java/co/mde/android/tddexample/LoginInteractor.java
@@ -1,4 +1,9 @@
 package co.mde.android.tddexample;
 
 public interface LoginInteractor {
+  void auth(String email, String password, Callback callback);
+
+  interface Callback {
+    void onFailure();
+  }
 }
